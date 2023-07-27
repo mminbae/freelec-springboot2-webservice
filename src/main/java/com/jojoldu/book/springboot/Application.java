6399b1af961949bdf7641a7c46f89d5c0b804588
min <min.Bae@creadto.com>
2023-07-27 18:25:50 +0900

    DEPLOY 6

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,14 +2,10 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.addListeners(new ApplicationPidFileWriter());
-        application.run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
